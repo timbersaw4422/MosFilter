4663638df09a7c02657f2ea983122347d021df71
timbersaw4422 <timbersaw442211@gmail.com>
2020-08-08 18:38:39 +0300

    added loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
-import '../global.css'
+import NextNprogress from 'nextjs-progressbar';
+import '../global.css';
 
 export default function MyApp({ Component, pageProps }) {
-  
-  return <Component {...pageProps} />
+
+  return (
+    <>
+      <NextNprogress
+        color="#4862D2"
+        height="3"
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
