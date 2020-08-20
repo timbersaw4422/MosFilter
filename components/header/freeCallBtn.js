e2e5cf852f99e55db52eadfd49998e386e56d2bd
timbersaw4422 <timbersaw442211@gmail.com>
2020-08-20 19:19:22 +0300

    added modal

--- components/header/freeCallBtn.js
+++ components/header/freeCallBtn.js
@@ -1,29 +1,39 @@
+import FreeCallModal from "../modal/freeCallModal";
+import {useState} from "react";
+
 const FreeCallBtn = () => {
+
+  const [isModalOpen, setModalOpen] = useState(false);
+
   return(
-    <div className="header__call-btn">
-       Бесплатный звонок
+    <>
+      {isModalOpen ? <FreeCallModal modalOpen = {setModalOpen} /> : null}
 
-       <style jsx>{`
-         .header__call-btn{
-            width:21rem;
-            height:4.5rem;
-            display:flex;
-            align-items:center;
-            justify-content:center;
-            font-weight: 600;
-            font-size: 14px;
-            color: #FFFFFF;
-            cursor:pointer;
-            background: linear-gradient(180deg, #6089F3 0%, #191792 100%);
-            box-shadow: 0px 4px 10px rgba(45, 82, 214, 0.41);
-            border-radius: 1rem;
-         }
+      <div className="header__call-btn" onClick = {() => setModalOpen(true)}>
+         Бесплатный звонок
+      </div>
 
-         @media (hover:hover){
+      <style jsx>{`
+        .header__call-btn{
+           width:21rem;
+           height:4.5rem;
+           display:flex;
+           align-items:center;
+           justify-content:center;
+           font-weight: 600;
+           font-size: 14px;
+           color: #FFFFFF;
+           cursor:pointer;
+           background: linear-gradient(180deg, #6089F3 0%, #191792 100%);
+           box-shadow: 0px 4px 10px rgba(45, 82, 214, 0.41);
+           border-radius: 1rem;
+        }
 
-         }
-       `}</style>
-    </div>
+        @media (hover:hover){
+
+        }
+      `}</style>
+    </>
   )
 }
 
