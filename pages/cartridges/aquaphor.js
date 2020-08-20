5c5aaceb0056b4056a430d891022887ba461f842
timbersaw4422 <timbersaw442211@gmail.com>
2020-08-20 14:36:52 +0300

    fixed bugs

--- pages/cartridges/aquaphor.js
+++ pages/cartridges/aquaphor.js
@@ -8,7 +8,7 @@ import WhatsApp from "../../components/whatsApp";
 import Link from "next/link";
 import Catalog from "../../components/catalog/catalog";
 import Calculator from "../../components/calculator/calculator";
-import AboutCartridgesAquaphor from "../../components/aboutBrands/aboutBrandboutCartridgesAquaphor";
+import AboutCartridgesAquaphor from "../../components/aboutBrands/aboutCartridgesAquaphor";
 
 
 
