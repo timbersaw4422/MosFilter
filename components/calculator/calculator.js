4a1cc444266979fcd2aa01e64ab6b3d513965232
timbersaw4422 <timbersaw442211@gmail.com>
2020-10-02 16:31:40 +0300

    font-awesome символ рубля

--- components/calculator/calculator.js
+++ components/calculator/calculator.js
@@ -101,7 +101,7 @@ const Calculator = () => {
                 <p className="calculator__option"><span>Тип фильтра: </span> {option2}</p>
                 <p className="calculator__option"><span>Замена картриджей: </span> {option3}</p>
                 <p className="calculator__option"><span>Местоположение: </span> {option4}</p>
-                <div className="calculator__option always-visible"><span>Стоимость: </span> <span className="calculator__price">{price} &#x20bd;</span></div>
+                <div className="calculator__option always-visible"><span>Стоимость: </span> <span className="calculator__price">{price} <i className="fas fa-ruble-sign"></i></span> </div>
                 <div className="call-btn" onClick = {() => setModalOpen(!isModalOpen)}>
                    <span>Оформить заказ </span>
                 </div>
