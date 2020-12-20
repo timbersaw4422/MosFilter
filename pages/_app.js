add511fd5c51883a1253bcfdc1883eedefa758e7
timbersaw4422 <timbersaw442211@gmail.com>
2020-12-20 13:59:57 +0300

    добавлена метрика

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Preloader from "../components/preloader";
 import {Component} from "react";
 import Cookies from "js-cookie";
 import cookies from 'next-cookies';
-import Head from "next/head";
+import {useEffect} from "react";
 
 import '../normalize.css';
 import '../global.css';
@@ -21,18 +21,22 @@ export default function MyApp({ Component, pageProps, isLoaderShowed }) {
 
   // <--Метрика-->
 
-    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+  useEffect(()=>{
+      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+     m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-   ym(55721440, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-   });
+     ym(55721440, "init", {
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true,
+          webvisor:true
+     });
+  } ,[])
 
-  // <--Метрика--> 
+
+
+  // <--Метрика-->
 
   return (
     <>
